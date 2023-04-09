cbbd46857fd0b013125e041243f4569121f781dc
Sargam <sargampoudel100@gmail.com>
2023-04-09 11:13:33 +0530

    :wrench: fix(config): added google colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        google: {
+          blue: "#428eff",
+          red: "#ff5145",
+          yellow: "#ffbb01",
+          green: "#38a852",
+          black: "#202124",
+        },
+      },
+    },
     fontFamily: {
-      roboto: ['Roboto', 'sans-serif'],
-      fraunces: ['Fraunces', 'serif'],
-      mulish: ['Mulish', 'sans-serif']
-    }
+      roboto: ["Roboto", "sans-serif"],
+      fraunces: ["Fraunces", "serif"],
+      mulish: ["Mulish", "sans-serif"],
+    },
   },
   plugins: [],
 };
