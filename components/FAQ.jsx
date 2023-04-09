682542c05928426a3e3ba3dde1985545c89b70cb
biratdatta <birat.datta26@gmail.com>
2023-04-09 23:40:59 +0530

    :memo: Added Testimonies

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -4,28 +4,23 @@ import Faq from "react-faq-component";
 function FAQ() {
   const data = {
     rows: [
+        {
+            title: "Who should join Google Developer Student Clubs FET-JU ?",
+            content: "All College and university  students are encouraged to join Google Developer Student Clubs FET-JU.",
+        },
+        {
+            title: " What are the benefits of joining GDSC FET-JU ? ",
+            content:
+                "Joining Google Developer Student Clubs FET-JU offers a supportive community, practical skills through workshops and hands-on projects, and career development opportunities. ",
+        },
+        
       {
-        title: "Lorem ipsum dolor sit amet,",
-        content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
-              ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
-              In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
-              Fusce sed commodo purus, at tempus turpis.`,
+        title: "Do I have to pay for joining this club?",
+        content: `   No, It is  free for everyone and you can always join free of cost.`,
       },
       {
-        title: "Nunc maximus, magna at ultricies elementum",
-        content:
-          "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
-      },
-      {
-        title: "Curabitur laoreet, mauris vel blandit fringilla",
-        content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim.`,
-      },
-      {
-        title: "What is the package version",
-        content: <p>current version is 1.2.1</p>,
+        title: "How is GDSC different from other clubs in the campus? ",
+        content: <p> If You Know You Know !! </p>,
       },
     ],
   };
