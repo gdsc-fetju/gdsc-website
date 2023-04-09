8e0f0063c3c2d3ae20303b27610010a5af7eefe0
Ankit Jha <jhaa11701@gmail.com>
2023-04-09 20:32:57 +0530

    :truck: chore

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Google Developer Students Clubs - FETJU</title>
+        <title>GDSC FETJU</title>
         <meta
           name="description"
           content="At Google Developer Student Clubs FET- Jain University we as a university based community functioning with an aim to promote peer-to-peer learning with an active usage of tools and technologies offered by Google. Here in the community, we learn and further our developers skills with an intent to abide by the idea of putting knowledge to practical use."
         />
+        <meta name="robots" content="index, follow" />
+
+
         <meta property="og:image" content="gdsc.png" />
         <meta
           name="keywords"
           content="gdscfetju,fetju,gdsc,Google Developer Students Clubs - FETJU, jain university, jain"
-        />        <meta property="og:type" content="website" />
+        />
+        <meta property="og:type" content="website" />
         <meta
           property="og:url"
           content="https://gdscju.tech/"
