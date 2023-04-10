cc4abd69c31de847d31d8368c327449c2dff4503
Ankit Jha <jhaa11701@gmail.com>
2023-04-10 12:52:33 +0530

    :sparkles: chore: added links in url

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,22 @@ import Image from "next/image";
 import React from "react";
 
 function Footer() {
+
+  const footerData = [
+    {
+      imageName: 'instagram',
+      url: 'https://www.instagram.com/gdscfetju'
+    },
+    {
+      imageName: 'linkedin',
+      url: 'https://www.linkedin.com/company/gdsc-fet-ju/'
+    },
+    {
+      imageName: 'twitter',
+      url: '/'
+    }
+  ]
+
   return (
     <section className="p-10 m-10 rounded-3xl border-google-blue border-2 text-center md:flex items-center justify-around md:p-15">
       {/* left column */}
@@ -23,9 +39,9 @@ function Footer() {
       <div className="middle md:w-1/3">
         <p className="text-google-green text-3xl font-bold">Follow Us</p>
         <div className="links flex justify-center mb-5">
-          <FooterImage imageName={"instagram"} />
-          <FooterImage imageName={"linkedin"} />
-          <FooterImage imageName={"twitter"} />
+          {
+            footerData.map((data, idx) => <FooterImage imageName={data.imageName} key={idx} url={data.url} />)
+          }
         </div>
         <h1 className="text-3xl font-bold text-google-blue mb-3">Contact Us</h1>
 
@@ -47,13 +63,14 @@ function Footer() {
   );
 }
 
-function FooterImage({ imageName }) {
+function FooterImage({ imageName, url }) {
   return (
     <span>
       <Image
         className="text-gray-500 m-2 cursor-pointer"
         src={`/social/${imageName}.svg`}
         height={25}
+        onClick={() => window.open(url, '_blank')}
         width={25}
         alt={`${imageName} icon`}
       />
