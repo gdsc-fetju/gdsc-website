6130afd807b171ae461e4e50b75a44552fba0176
Ankit Jha <jhaa11701@gmail.com>
2023-04-10 16:32:22 +0530

    :memo: docs: added new testimonial

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -7,7 +7,33 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import TestimonialCard from "./TestimonialCard";
+const testimonialData = [
 
+  {
+    name: 'Tushar Vilekar',
+    content: `The outreach efforts of GDSC FET JU have been exceptional, with the club's activities having a tangible impact on the wider community.The 2022-23 session saw a lot of active and successful collaborations with GDSC FET JU in terms of Organizers CompeteNCompose (An online hackathon on a national level) and National Organizers ofGDSC WoW(Wonder of Wonders).The club has taken an active role in organizing events, workshops, and seminars, and I am confident that the club's efforts will continue to bear fruit in the years to come.`,
+    tags: ['GDSC Lead LPU', 'Developer', 'Student'],
+    src: '/testimonialImages/tushar.jpg',
+    url: 'https://www.linkedin.com/in/tushar-vilekar-8556701b3/'
+  },
+  {
+    name: 'Prnika Bakshi',
+    content: `I remember how it started and how it’s going now I feel really happy that our university have got a cut edge ecosystem wherein all the students have got the opportunity to get that culture,they are learning new things which will help them to be fit in this competitive environment. This year GDSC group has worked a lot I really would like to appreciate Birat and his whole team who has helped in achieving this bar especially in our university the way all the sessions every month used to be organised and lot many things have given an exposure to all the students to learn not only coding but much more than that! Looking forward for this group to grow more in coming years to make our university more proud with all rounder students.`,
+    tags: ['GDSC Lead', 'Microsoft'],
+    src: '/testimonialImages/prnika.jpeg',
+    url: 'https://www.linkedin.com/in/prnika-bakshi-562654167'
+  },
+  {
+    name: 'Bhumika Chopra',
+    content: `The potential this community offers are limitless. The ongoing encouragement, inspiration, and support from such outstanding people make the experience worthwhile. Working with such excellent leaders, and organising events and hackathons together is simply incredible.`,
+    tags: ['GDSC LEAD MAIT'],
+    src: '/testimonialImages/bhumika.jpg',
+    url: 'https://www.linkedin.com/in/bhumika-chopra-673951203'
+  },
+]
+
+
+const colors = [`#428eff`, `#ff5145`, `#ffbb01`];
 export default function Testimonial() {
   return (
     <>
@@ -19,30 +45,19 @@ export default function Testimonial() {
           modules={[Navigation, Pagination, Autoplay]}
           spaceBetween={0}
           slidesPerView={1}
+          loop={true}
           navigation
           draggable={false}
           pagination
-          grabCursor={true}
+          grabCursor={false}
           autoplay={{ delay: 2500 }}
         >
-          <SwiperSlide>
-            <TestimonialCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TestimonialCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TestimonialCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TestimonialCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TestimonialCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <TestimonialCard />
-          </SwiperSlide>
+          {testimonialData.map((data, idx) =>
+            <SwiperSlide key={idx} >
+              <TestimonialCard {...data} borderColor={colors[idx % colors.length]} />
+            </SwiperSlide>
+          )}
+
         </Swiper>
       </div>
     </>
--- components/TestimonialCard.jsx
+++ components/TestimonialCard.jsx
@@ -1,37 +1,36 @@
+import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { BsLinkedin } from 'react-icons/bs'
 
-function TestimonialCard() {
+function TestimonialCard({ name, src, content, tags, url }) {
   return (
-    <section className="flex flex-col justify-center md:flex-row items-center py-10 mx-10 md:mx-56  md:border-google-blue md:border-2 md:p-5 my-10 rounded-xl">
+    <section className={`flex flex-col justify-center md:flex-row items-center py-10 mx-10 md:mx-56  md:border-google-blue md:border-2 md:p-5 my-10 rounded-xl`}>
       {/* Left Section */}
       <div>
         <Image
-          src="/testimonialImages/tushar.jpg"
+          src={src}
           width={200}
           height={200}
           className="rounded-full object-center aspect-square"
-          alt="image"
+          alt={`${name} image`}
         ></Image>
       </div>
       {/* Right Section */}
       <div className="flex flex-col md:items-start items-center">
-        <span className="text-2xl font-semibold">Tushar Vilekar</span>
-        <div className="m-3 flex gap-4">
-          <div>
-            <span className="my-1 text-[#4E8E70] px-3 py-1 bg-[#C3EAD7] rounded-md font-mulish">
-              GDSC Lead LPU
-            </span>
-          </div>
-          <div>
-            <span className="my-1 text-[#62989C] px-3 py-1 bg-[#C3E7EA] rounded-md font-mulish">
-              Developer
-            </span>
-          </div>
+        <span className="text-2xl font-semibold">{name}</span>
+        <div className="m-3 flex gap-4 items-center">
+          {tags.map((tag, idx) =>
+            <div key={idx} >
+              <span className="my-1 text-[#4E8E70] px-3 py-1 bg-[#C3EAD7] rounded-md font-mulish">
+                {tag}
+              </span>
+            </div>)}
+          <Link href={url}>
+            <BsLinkedin className="text-lg text-google-blue" />
+          </Link>
         </div>
-        <p className="px-5 text-justify max-w-xl">
-          &quot;The outreach efforts of GDSC FET JU have been exceptional, with the club&apos;s activities having a tangible impact on the wider community. The 2022-23 session saw a lot of active and successful collaborations with GDSC FET JU in terms of Organizers CompeteNCompose (An online hackathon on a national level) and National Organizers of GDSC WoW (Wonder of Wonders). The club has taken an active role in organizing events, workshops, and seminars, and I am confident that the club&apos;s efforts will continue to bear fruit in the years to come.
-          &quot;
-        </p>
+        <p className="px-5 text-justify max-w-xl">{content}</p>
       </div>
     </section>
   );
