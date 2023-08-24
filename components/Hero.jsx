9604b23d2c2e5692673b4cb5bbc88df7645beb9f
Sargam <sargampoudel100@gmail.com>
2023-08-24 13:46:30 +0530

    :pencil2: fix(links): change from linktree to links.gdsc

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,39 +1,39 @@
-import Image from "next/image";
-import React from "react";
-import heroImage from "../public/hero.gif";
-import Link from "next/link";
+import Image from 'next/image';
+import React from 'react';
+import heroImage from '../public/hero.gif';
+import Link from 'next/link';
 
 function Hero() {
   return (
-    <div className="flex justify-around items-center ">
-      <div id="about" className="relative bottom-64"></div>
-      <div className="p-10 md:p-0 md:w-1/2 w-full text-justify grid place-items-start place-content-center">
-        <div className="flex justify-center md:justify-around items-center p-10">
+    <div className='flex items-center justify-around '>
+      <div id='about' className='relative bottom-64'></div>
+      <div className='grid w-full p-10 text-justify md:p-0 md:w-1/2 place-items-start place-content-center'>
+        <div className='flex items-center justify-center p-10 md:justify-around'>
           <Image
-            className="sm:hidden"
-            src="/logo.png"
+            className='sm:hidden'
+            src='/logo.png'
             width={500}
             height={500}
-            alt="heroImage"
+            alt='heroImage'
           />
         </div>
-        <p className="font-normal text-base sm:text-lg md:text-xl lg:text-2xl tracking-wide leading-7 sm:leading-8 md:leading-9">
+        <p className='text-base font-normal leading-7 tracking-wide sm:text-lg md:text-xl lg:text-2xl sm:leading-8 md:leading-9'>
           At
-          <span className="text-google-blue font-semibold text-base sm:text-lg md:text-xl lg:text-2xl">
-            {" "}
+          <span className='text-base font-semibold text-google-blue sm:text-lg md:text-xl lg:text-2xl'>
+            {' '}
             Google
           </span>
-          <span className="text-google-green font-semibold text-base sm:text-lg md:text-xl lg:text-2xl">
-            {" "}
+          <span className='text-base font-semibold text-google-green sm:text-lg md:text-xl lg:text-2xl'>
+            {' '}
             Developer
           </span>
-          <span className="text-google-red font-semibold text-base sm:text-lg md:text-xl lg:text-2xl">
-            {" "}
+          <span className='text-base font-semibold text-google-red sm:text-lg md:text-xl lg:text-2xl'>
+            {' '}
             Student
           </span>
-          <span className="text-google-yellow font-semibold text-base sm:text-lg md:text-xl lg:text-2xl">
-            {" "}
-            Clubs{" "}
+          <span className='text-base font-semibold text-google-yellow sm:text-lg md:text-xl lg:text-2xl'>
+            {' '}
+            Clubs{' '}
           </span>
           FET- Jain University we as a university based community functioning
           with an aim to promote peer-to-peer learning with an active usage of
@@ -43,17 +43,17 @@ function Hero() {
         </p>
 
         <Link
-          href="https://linktr.ee/gdscfetju"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://links.gdscfetju.co/'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          <button className="bg-blue-500 hover:bg-blue-600 focus:outline-none text-white px-10 py-2 rounded-lg font-semibold mt-3 md:mt-5 border border-google-black flex items-center">
+          <button className='flex items-center px-10 py-2 mt-3 font-semibold text-white bg-blue-500 border rounded-lg hover:bg-blue-600 focus:outline-none md:mt-5 border-google-black'>
             Join Us
           </button>
         </Link>
       </div>
-      <div className="right w-1/3 md:block hidden">
-        <Image src={heroImage} alt={"hero image"} className="w-full h-full" />
+      <div className='hidden w-1/3 right md:block'>
+        <Image src={heroImage} alt={'hero image'} className='w-full h-full' />
       </div>
     </div>
   );
